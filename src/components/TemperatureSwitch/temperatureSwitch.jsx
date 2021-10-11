7908732c023dd809515e8d460220895007c0b9ab
Falxns <maks.falonse@gmail.com>
2021-10-11 15:35:06 +0300

    feat: implement temperature switch

--- src/components/TemperatureSwitch/temperatureSwitch.jsx
+++ src/components/TemperatureSwitch/temperatureSwitch.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./temperatureSwitch.css";
 
 function TemperatureSwitch() {
-  return <></>;
+  function handleSwitch(e) {
+    console.log(e.target.checked);
+  }
+
+  return (
+    <div className="temperature-switch">
+      <p className="temperature-switch__p">°C</p>
+      <label className="temperature-switch__switch switch">
+        <input
+          className="switch__input"
+          type="checkbox"
+          onClick={handleSwitch}
+        />
+        <span className="switch__slider"></span>
+      </label>
+      <p className="temperature-switch__p">℉</p>
+    </div>
+  );
 }
 
 export default TemperatureSwitch;
