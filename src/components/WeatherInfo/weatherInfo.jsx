2e5b0b089bc0ed83691b0d4bf3b5674f652d1321
Falxns <maks.falonse@gmail.com>
2021-10-19 09:53:42 +0300

    fix: fix console error with table

--- src/components/WeatherInfo/weatherInfo.jsx
+++ src/components/WeatherInfo/weatherInfo.jsx
@@ -4,34 +4,40 @@ import "./weatherInfo.css";
 function WeatherInfo() {
   return (
     <table className="weather-info">
-      <tr className="weather-info__tr">
-        <th className="weather-info__th weather-info__city-label">Location</th>
-        <th className="weather-info__th weather-info__temperature-label">
-          Temperature
-        </th>
-        <th className="weather-info__th weather-info__cloudiness-label">
-          Cloudiness
-        </th>
-        <th className="weather-info__th weather-info__wind-label">Wind</th>
-        <th className="weather-info__th weather-info__pressure-label">
-          Pressure
-        </th>
-        <th className="weather-info__th weather-info__humidity-label">
-          Humidity
-        </th>
-      </tr>
-      <tr className="weather-info__tr">
-        <td className="weather-info__td weather-info__city">Minsk</td>
-        <td className="weather-info__td weather-info__temperature">27 C</td>
-        <td className="weather-info__td weather-info__cloudiness">
-          light rain
-        </td>
-        <td className="weather-info__td weather-info__wind">
-          North-East, 4.57 m/s
-        </td>
-        <td className="weather-info__td weather-info__pressure">1008 hpa</td>
-        <td className="weather-info__td weather-info__humidity">80%</td>
-      </tr>
+      <thead>
+        <tr className="weather-info__tr">
+          <th className="weather-info__th weather-info__city-label">
+            Location
+          </th>
+          <th className="weather-info__th weather-info__temperature-label">
+            Temperature
+          </th>
+          <th className="weather-info__th weather-info__cloudiness-label">
+            Cloudiness
+          </th>
+          <th className="weather-info__th weather-info__wind-label">Wind</th>
+          <th className="weather-info__th weather-info__pressure-label">
+            Pressure
+          </th>
+          <th className="weather-info__th weather-info__humidity-label">
+            Humidity
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="weather-info__tr">
+          <td className="weather-info__td weather-info__city">Minsk</td>
+          <td className="weather-info__td weather-info__temperature">27 C</td>
+          <td className="weather-info__td weather-info__cloudiness">
+            light rain
+          </td>
+          <td className="weather-info__td weather-info__wind">
+            North-East, 4.57 m/s
+          </td>
+          <td className="weather-info__td weather-info__pressure">1008 hpa</td>
+          <td className="weather-info__td weather-info__humidity">80%</td>
+        </tr>
+      </tbody>
     </table>
   );
 }
