a67daf8dda64354c0fcb171aa15f6ee973958c1c
Falxns <maks.falonse@gmail.com>
2021-10-26 11:14:51 +0300

    feat: create context for temperature type

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
 import Main from './components/Main/main';
+import temperatureContext from './context/temperature.context';
 
 function App() {
+  const [temperature, setTemperature] = useState(false);
+
   return (
-    <>
+    <temperatureContext.Provider value={{ temperature, setTemperature }}>
       <Header />
       <Main />
       <Footer />
-    </>
+    </temperatureContext.Provider>
   );
 }
 
--- src/components/TemperatureSwitch/temperatureSwitch.jsx
+++ src/components/TemperatureSwitch/temperatureSwitch.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './temperatureSwitch.scss';
+import temperatureContext from '../../context/temperature.context';
 
 function TemperatureSwitch() {
+  const { setTemperature } = useContext(temperatureContext);
+  const handleTemperatureChange = (event) => {
+    setTemperature(event.target.checked);
+  };
+
   return (
     <div className="temperature-switch">
       <p className="temperature-switch__p">°C</p>
       <label className="temperature-switch__switch switch" htmlFor="switch">
-        <input className="switch__input" type="checkbox" id="switch" />
+        <input
+          className="switch__input"
+          type="checkbox"
+          id="switch"
+          onChange={handleTemperatureChange}
+        />
         <span className="switch__slider" />
       </label>
       <p className="temperature-switch__p">℉</p>
