1e4cd3835fca6ed55acbaa0e9dfeaa77267039d7
Falxns <maks.falonse@gmail.com>
2021-10-26 14:28:09 +0300

    feat: add button to search

--- src/components/SearchBar/searchBar.jsx
+++ src/components/SearchBar/searchBar.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './searchBar.scss';
+import plus from '../../assets/icons/plus.svg';
 
 const SearchBar = () => (
-  <input
-    className="header__search"
-    type="search"
-    minLength="3"
-    placeholder="Enter a city"
-  />
+  <div className="header__search">
+    <input
+      className="search__input"
+      type="search"
+      minLength="3"
+      placeholder="Enter a city"
+    />
+    <button className="search__btn" type="submit">
+      <img className="search__img" src={plus} alt="+" />
+    </button>
+  </div>
 );
 
 export default SearchBar;
